6d37bdd260f34647f02b839f86fb131ffaaa08eb
枫树 <2359427041@qq.com>
2026-02-22 00:24:41 +0800

    Update and rename bisai11.py to model train.py

--- model train.py
+++ model train.py
@@ -3,19 +3,22 @@
 import torchvision
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
-from modelll import *
+from torch import nn
+import torch
 from torch.utils.data import DataLoader
 import os
 
 #读取数据集
 transform=transforms.Compose([
-    transforms.Resize((32, 32)),  #调整尺寸为 32×32
+    transforms.Resize((64, 48)),  #调整尺寸
     transforms.ToTensor(),  #PIL转张量，像素值缩放到[0, 1]，形状变为 (3, 32, 32)
     transforms.Normalize((0.5,), (0.5,)),  # 步骤4：3 通道标准化（配套修改）
 ])
 
 train_data = torchvision.datasets.ImageFolder(os.path.join("traindir"),transform=transform)
 test_data = torchvision.datasets.ImageFolder(os.path.join("testdir"),transform=transform)
+test_data_size=len(test_data)
+
 test_data_size=len(test_data)
 
 #读取数据集
@@ -25,47 +28,53 @@ test_loader=DataLoader(test_data,batch_size=64,shuffle=True)
 #实例化网络模型
 class Discern(nn.Module):
     def __init__(self):
-        super(Discern,self).__init__()
-        self.model=torch.nn.Sequential(
-            nn.Conv2d(in_channels=3,out_channels=32,kernel_size=5,stride=1,padding=2),
-            nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Conv2d(in_channels=32,out_channels=32,kernel_size=5,stride=1,padding=2),
-            nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Conv2d(in_channels=32,out_channels=64,kernel_size=5,stride=1,padding=2),
-            nn.MaxPool2d(kernel_size=2),
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+
             nn.Flatten(),
-            nn.Linear(in_features=64*4*4,out_features=64),
-            nn.Linear(in_features=64,out_features=4)
+            nn.Linear(128 * 8 * 6, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(128, 4)
         )
+
     def forward(self,x):
         x=self.model(x)
         return x
 fs=Discern()
-fs=fs.cuda()
 
 #损失函数
 loss_func=nn.CrossEntropyLoss()
-loss_func=loss_func.cuda()
 #优化器
 optimizer=torch.optim.SGD(fs.parameters(),lr=0.001,momentum=0.9)
 
 writer=SummaryWriter("log")
 
 #设置训练参数
-epochs=100 #10次
+epochs=20 #10次
 step=0
 for i in range(epochs):
     for data in train_loader:
         img,label=data #图片和标签
-        img=img.cuda()
-        label=label.cuda()
         output=fs(img)
         loss=loss_func(output,label)
-
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
         step=step + 1
         #print(step,loss.item())
 
@@ -75,8 +84,6 @@ for i in range(epochs):
     with torch.no_grad():
         for data in test_loader:
             img,label=data
-            img=img.cuda()
-            label=label.cuda()
             output=fs(img)
             loss=loss_func(output,label)
             total_test_loss=total_test_loss+loss.item()
@@ -85,11 +92,11 @@ for i in range(epochs):
 
     #print("Loss{}",format(total_test_loss,".2f"))
     #print("Accuracy{}",format(total_accuracy/test_data_size,".2f"))
-    writer.add_scalar("loss",total_test_loss,step)
-    writer.add_scalar("accuracy",total_accuracy/test_data_size,step)
+    writer.add_scalar("loss2",total_test_loss,step)
+    writer.add_scalar("accuracy1",total_accuracy/test_data_size,step)
     step+=1
 
-    torch.save(fs.state_dict(),"FS_model_comp.pth")
+    torch.save(fs.state_dict(),"model_comp_color.pth")
     print("已保存")
 
 writer.close()
